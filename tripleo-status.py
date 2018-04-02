44f1cddef15d1f24426887fc357f7c9fe1d3c0fe
Enrique Llorente <quique.llorente@gmail.com>
2018-04-02 15:03:01 +0200

    Add #oooq !getstatus

--- tripleo-status.py
+++ tripleo-status.py
@@ -3,12 +3,17 @@ import requests
 import re
 import pprint
 import json
+import irc.client
+import itertools
+import sys
 
 from bs4 import BeautifulSoup
 from datetime import datetime 
 from launchpadlib.launchpad import Launchpad
 
 infra_status_regexp = re.compile('^ *([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}) *UTC *(.+)$')
+failing_check_jobs = re.compile('^FAILING CHECK JOBS: .*')
+
 infra_status_url = 'https://wiki.openstack.org/wiki/Infrastructure_Status'
 upstream_zuul_url = 'http://zuul.openstack.org/status'
 
@@ -39,13 +44,55 @@ def get_upstream_tripleo_gate():
 def get_upstream_tripleo_bugs(since):
     launchpad = Launchpad.login_anonymously('OOOQ Ruck Rover', 'production', cachedir, version='devel')
     project = launchpad.projects['tripleo']
+
+    # We can filter by status too 
     bugs = project.searchTasks(created_since = since)
+    
     return bugs
     #browsed_bugs = []
     #for bug in bugs:
     #    browser = launchpad._browser
     #    browsed_bugs.append(browser.get(bug.self_link))
     #return browsed_bugs
+
+def get_oooq_status():
+    client = irc.client.Reactor()
+    server = client.server()
+    
+
+    def on_connect(connection, event):
+        connection.join('#oooq')
+    
+    def on_join(connection, event):
+        connection.privmsg('#oooq', '!gatestatus')
+
+    def on_disconmnect(connection, event):
+        raise SystemExit()
+    
+    def on_public_message(connection, event):
+        message = event.arguments[0]
+        # TODO: Check it's hubbot
+        if failing_check_jobs.match(message):
+            print("hubbot: {}".format(message))
+            connection.quit("Using irc.client.py")
+
+    try:
+        server.connect("chat.freenode.net", 6667, "ruck-rover-bot")
+    except irc.client.ServerConnectionError:
+        print(sys.exc_info()[1])
+        raise SystemExit(1)
+    
+    client.add_global_handler("welcome", on_connect)
+    client.add_global_handler("join", on_join)
+    client.add_global_handler("disconnect", on_disconmnect)
+    client.add_global_handler("pubmsg", on_public_message)
+
+    try:
+        client.process_forever()
+    except SystemExit:
+        print('END')
+
+    return "TODO"
 
 def filter_infra_issues_by_date(date):
     issues = get_infra_issues()
@@ -55,12 +102,17 @@ def filter_infra_issues_by_date(date):
 def main():
     since_date=datetime(2018, 3, 13)
     status = {}
-    status['infra-issues'] = filter_infra_issues_by_date(since_date)
-    status['upstream-tripleo-gate'] = get_upstream_tripleo_gate()
-    status['upstream-tripleo-bugs'] = get_upstream_tripleo_bugs(since=since_date)
-    print("Number of infra issues: {}".format(len(status['infra-issues'])))
-    print("Size of tripleo gate queue: {}".format(len(status['upstream-tripleo-gate'])))
-    print("Number of tripleo bugs: {}".format(len(status['upstream-tripleo-bugs'])))
+    #TODO: Check #tripleo for <ooolpbot> URGENT TRIPLEO TASKS NEED ATTENTION
+    #TODO: Check #oooq; ! gatestatus
+    #TODO: http://www.rssmix.com/u/8262477/rss.xml
+    #status['infra-issues'] = filter_infra_issues_by_date(since_date)
+    #status['upstream-tripleo-gate'] = get_upstream_tripleo_gate()
+    #status['upstream-tripleo-bugs'] = get_upstream_tripleo_bugs(since=since_date)
+    status['oooq-status'] = get_oooq_status()
+    #print("Number of infra issues: {}".format(len(status['infra-issues'])))
+    #print("Size of tripleo gate queue: {}".format(len(status['upstream-tripleo-gate'])))
+    #print("Number of tripleo bugs: {}".format(len(status['upstream-tripleo-bugs'])))
+    print("OOOQ status: {}".format(len(status['oooq-status'])))
 
 if __name__ == '__main__':
     main()
